151749f1b958a4c21e49bf8b0777aeca7a64ae79
João Victor <themaciel190@gmail.com>
2024-05-30 20:52:01 -0500

    add hooks and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Header from "./Components/Header";
+import Sidenav from "./Components/Sidenav";
+import Resumo from "./Pages/Resumo";
 import "./Style.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Sidenav />
+        <Resumo />
+      </main>
+    </>
+  );
 }
 
 export default App;
